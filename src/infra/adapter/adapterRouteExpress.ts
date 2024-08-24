47ea323f8ad6fddda2c258b2e3a2ed6cacea8db4
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-08-24 19:06:55 -0300

    feat(adapterRouteExpress): add express adapter
    Added express adapter for dynamic server changing

---
+++ src/infra/adapter/adapterRouteExpress.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { HttpRequest } from "../shared/protocols/http";
+import { IController } from "../shared/protocols/controller";
+
+export const adapterRouteExpress = (controller: IController) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+    };
+
+    const httpResponse = await controller.handle(httpRequest);
+    res.status(httpResponse.statusCode).json(httpResponse.body);
+  };
+};
